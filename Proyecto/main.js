1f8f8e17fa3bc787f9986356cc5385e2316c456c
Santiago Barale <sbarale03@gmail.com>
2021-10-08 15:17:16 -0300

    Solucion de problemas

--- Proyecto/main.js
+++ Proyecto/main.js
@@ -1,16 +1,25 @@
-function res(){
-    var atq = document.getElementById("atq").value;
+    function res(){
+        var atq = document.getElementById("atq").value;
 
-    var def = document.getElementById("def").value;
+        var def = document.getElementById("def").value;
 
-    var ps = document.getElementById("ps").value;
+        var ps = document.getElementById("ps").value;
 
-    var rest = (( parseInt(atq) + parseInt(def) + parseInt(ps) ) * 100) / 45;
+        var rest = (( parseInt(atq) + parseInt(def) + parseInt(ps) ) * 100) / 45;
 
-    var rest = Math.ceil(rest)
+        var rest = Math.ceil(rest);
 
-    // El "parseInt()" sirve para convertir en entero un numero //
-    // El "Math.ceil()" sirve para redondear para arriba //
+        // El "document.getElementById()" sirve para llamar un id de html //
+        // El "parseInt()" sirve para convertir en entero un numero //
+        // El "Math.ceil()" sirve para redondear para arriba //
 
-    document.write("Resultado = " + rest + "%");
-}
+        if(0 <= rest <= 100) {
+
+            document.getElementById("rf").innerHTML="Resultado: " + rest + "%";
+
+        } else {
+
+            document.getElementById("rf").innerHTML="Ingresaste numeros incorrectos. Por favor intentalo de nuevo, pero esta vez ingresando numeros entre 0-15.";
+        }
+    
+    }
